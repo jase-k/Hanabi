11c29ed78828a9f9d6b138c5575f1cae110114d8
Glitch (hello-sqlite) <none>
2019-01-10 14:50:29 +0000

    🚝📌 Checkpoint ./views/index.html:1326932/7010 ./public/style.css:1326932/2529 ./public/client.js:1326932/4922

--- public/client.js
+++ public/client.js
@@ -1,49 +1,118 @@
-// client-side js
-// run by the browser each time your view template referencing it is loaded
+//Retrieves the Select Button From index.html
+const selectedPlayers = document.getElementById('select');
+const optionalCard = document.getElementById('optional-card');
+const select = document.getElementById('game-init');
+const allCards = document.getElementById('cardHolder');
+const pdrag = document.getElementById('drag');
+const hideDiv = document.getElementById('hold');
 
-console.log('hello world :o');
-
-let dreams = [];
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function to call when our request for dreams is done
-const getDreamsListener = function() {
-  // parse our response to convert to JSON
-  dreams = JSON.parse(this.responseText);
-
-  // iterate through every dream and add it to our page
-  dreams.forEach( function(row) {
-    appendNewDream(row.dream);
-  });
-}
-
-// request the dreams from our app's sqlite database
-const dreamRequest = new XMLHttpRequest();
-dreamRequest.onload = getDreamsListener;
-dreamRequest.open('get', '/getDreams');
-dreamRequest.send();
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
-}
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
+function hideClass(element){
+  console.log(element);
+  element.classList.add("hide")
 };
+
+function toggleClass(element){
+  console.log('toggling Class')
+  element.classList.toggle("hide")
+}
+
+selectedPlayers.onchange = (event) =>{
+  console.log('This should be 1-5:', event.target.value)
+  if(event.target.value == 4 || event.target.value == 5){
+    console.log('Adding Hide Class to Card5');
+    optionalCard.style.display = "none";
+    toggleClass(select);
+    toggleClass(allCards);  
+    toggleClass(instDiv);
+    toggleClass(hideDiv);
+  } else{
+      toggleClass(select);
+      toggleClass(allCards);
+      toggleClass(instDiv);
+      toggleClass(hideDiv);
+  }
+}
+
+document.ondragstart = (event) =>{
+ // handleDragStart(event)
+  console.log(event)
+   console.log("dragStart");
+  
+  //Get Element's ID: and whole Element
+  var id = event.srcElement.id
+  var element = document.getElementById(id)
+  
+  // Change the source element's background color to signify drag has started
+ element.style.border = "blue";
+ 
+  // Set the drag's format and data. Use the event target's id for the data 
+ event.dataTransfer.setData("text/plain", id);
+  console.log(id, element)
+  
+};
+
+document.ondragover = (event) => {
+ event.preventDefault();
+  console.log("Something is being Drug") }
+
+
+///////////////////////////////
+// Below Creates Drop Events in Each Card Space
+////////////////////////////////
+var cards = [];
+cards.push(document.getElementById("card1"))
+cards.push(document.getElementById("card2"))
+cards.push(document.getElementById("card3"))
+cards.push(document.getElementById("card4"))
+cards.push(document.getElementById("card5"))
+
+cards.forEach(card =>
+  card.ondrop = (event) => {
+   event.preventDefault();
+ // handleDrop(event)
+ console.log("Drop");
+  
+ event.preventDefault();
+ // Get the data, which is the id of the drop target
+  
+ var data = event.dataTransfer.getData("text");
+  if(event.target.nodeName == "DIV"){
+     event.target.appendChild(document.getElementById(data));
+    console.log(event)
+  //Adding the Child back into the hold 'DIV'
+    var newp = document.createElement("p")
+    var hold = document.getElementById("hold");
+      newp.innerHTML = event.toElement.lastChild.innerText
+      newp.draggable = "true"
+      newp.id = Math.floor(Math.random()*1000)
+      console.log(newp)
+      hold.appendChild(newp);
+  }
+  
+// Clear the drag data cache (for all formats/types)
+ event.dataTransfer.clearData();
+}
+              );
+
+const buttons = document.getElementsByClassName('button')
+const instructionButton = document.getElementById('instructions-button')
+const instDiv =  document.getElementById('instructions')
+
+console.log(buttons)
+
+for(var i =0; i<buttons.length; i++){
+buttons[i].onclick = (event) => {
+    console.log("Button Was Clicked")
+    console.log(event)
+  var sibling = event.target.nextSibling.nextSibling
+  console.log(sibling)  
+  for(var i = 0; i < sibling.childNodes.length;){
+  sibling.removeChild(sibling.childNodes[i]);
+  }
+  }
+}
+
+instructionButton.onclick = (event) =>{
+  console.log("ButtonClicked", instDiv)
+  toggleClass(instDiv);
+}
