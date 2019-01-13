c7710b747464d4c249ec9607d9cca96611a61dc4
Glitch (hello-sqlite) <none>
2019-01-13 20:36:05 +0000

    🌊🔥 Checkpoint ./server.js:1326932/1054

--- server.js
+++ server.js
@@ -26,12 +26,17 @@ var table = 'card1, card2, card3, card4'
 
 
 db.serialize(() => { 
-  db.run('DROP TABLE IF EXISTS Hanabi_Games', error => {
+  db.run('DROP TABLE IF EXISTS HanabiGames', error => {
     if (error) {
       throw error;
     }
   })
-   db.run('DROP TABLE IF EXISTS Original_Deck', error => {
+   db.run('DROP TABLE IF EXISTS OriginalDeck', error => {
+    if (error) {
+      throw error;
+    }
+  })
+   db.run('DROP TABLE IF EXISTS PlayingDeck', error => {
     if (error) {
       throw error;
     }
@@ -51,28 +56,61 @@ db.serialize(() => {
       throw error;
     }
   })
-  db.run('CREATE TABLE Hanabi_Games (id TEXT PRIMARY KEY, numberOfPlayers INTEGER NOT NULL, dateCreated DATE, score INTEGER, originalDeckId INTEGER, playingDeckId INTEGER, discardedCardsId INTEGER, playedCardsId INTEGER, playersId INTEGER)');
-  db.run('CREATE TABLE Original_Deck(id INTEGER, gameId TEXT, card1 TEXT, card2 TEXT, card3 TEXT, card4 TEXT)');
-  db.run('CREATE TABLE Playing_Deck(id INTEGER)');
+  db.run('CREATE TABLE HanabiGames (id TEXT PRIMARY KEY, numberOfPlayers INTEGER NOT NULL, dateCreated DATE, score INTEGER, originalDeckId INTEGER, playingDeckId INTEGER, discardedCardsId INTEGER, playedCardsId INTEGER, playersId INTEGER)');
+  db.run('CREATE TABLE OriginalDeck(id INTEGER, gameId TEXT, card1 TEXT, card2 TEXT, card3 TEXT, card4 TEXT)');
+  db.run('CREATE TABLE PlayingDeck(id INTEGER)');
   db.run('CREATE TABLE DiscardedCards(id INTEGER)');
   db.run('CREATE TABLE PlayedCards(id INTEGER)');
-  db.run('CREATE TABLE Players(id INTEGER)');
-  db.run('INSERT INTO Original_Deck(id, gameId, '+table+') VALUES (0000, "sample", "red 5", "blue 3", "white 2", "green 2")');
-  db.run('INSERT INTO Hanabi_Games (id, numberOfPlayers, dateCreated, originalDeckId, playingDeckId, discardedCardsId, playedCardsId, playersId) VALUES("sample", 5, "2019-01-12T15:08:50.122Z", 0000, 0000, 0000, 0000, 0000)')
-  db.each('SELECT * from Hanabi_Games', function(err, row) {
+  db.run('CREATE TABLE Players(id INTEGER, gameId TEXT, name TEXT)');
+  db.run('INSERT INTO OriginalDeck(id, gameId, '+table+') VALUES (0000, "sample", "red 5", "blue 3", "white 2", "green 2")');
+  db.run('INSERT INTO HanabiGames (id, numberOfPlayers, dateCreated, originalDeckId, playingDeckId, discardedCardsId, playedCardsId, playersId) VALUES("sample", 5, "2019-01-12T15:08:50.122Z", 0000, 0000, 0000, 0000, 0000)')
+  db.each('SELECT * from HanabiGames', function(err, row) {
     console.log("Hanabi Table")  
     if ( row ) {
         console.log('record:', row);
       }
     });
-  db.each('SELECT * from Original_Deck', function(err, row) {
- console.log('Original_Deck')
+  db.each('SELECT * from OriginalDeck', function(err, row) {
+ console.log('OriginalDeck')
     if(err){
     throw err}
   if(row){
     console.log('record:', row) 
     }
   });
+  db.each('SELECT * from PlayingDeck', function(err, row) {
+ console.log('OriginalDeck')
+    if(err){
+    throw err}
+  if(row){
+    console.log('record:', row) 
+    }
+  });
+  db.each('SELECT * from DiscardedCards', function(err, row) {
+ console.log('DiscardedCards')
+    if(err){
+    throw err}
+  if(row){
+    console.log('record:', row) 
+    }
+  });
+  db.each('SELECT * from PlayedCards', function(err, row) {
+ console.log('PlayedCards')
+    if(err){
+    throw err}
+  if(row){
+    console.log('record:', row) 
+    }
+  });
+  db.each('SELECT * from Players', function(err, row) {
+ console.log('Players')
+    if(err){
+    throw err}
+  if(row){
+    console.log('record:', row) 
+    }
+  });
+  
 });
   
 
