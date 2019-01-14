17d7ca4af605eca98798b593f8f906fdc74b9c91
Glitch (hello-sqlite) <none>
2019-01-14 23:14:35 +0000

    🐜🏟 Checkpoint ./client/apicalls.js:1326932/422

--- client/apicalls.js
+++ client/apicalls.js
@@ -1,1 +1,10 @@
-fetch('https://puddle-catcher.glitch.me/newgame?players=3').then(
+fetch('https://puddle-catcher.glitch.me/newgame?players=3').then(response => {
+  if(response.ok){
+    return response.json();
+  }
+  throw new Error('Request failed');
+}, networkError => console.log(networkError.message)
+  ).then(jsonResponse => {
+  
+});
+                                                                 
