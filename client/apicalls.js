ab2b1f8ed936ceb02fa05861516b3e7be1c6b753
Glitch (hello-sqlite) <none>
2019-01-15 00:20:35 +0000

    👵😳 Checkpoint ./views/index.html:1326932/24 ./client/apicalls.js:1326932/2942

--- client/apicalls.js
+++ client/apicalls.js
@@ -1,7 +1,156 @@
 function helperFunction(object){
 
   return(
-  <>
+  <div class="container">
+      <div id="player1" class="teamatesHand">
+        <div>
+          Player 1
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            {object.players[0].hand[0].color}
+          </h3>
+          <h4>
+            {object.players[0].hand[0].number}
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+      </div>
+      <div id="player2" class="teamatesHand">
+        <div>
+          Player 2
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+      </div>
+      <div id="player3" class="teamatesHand">
+        <div>
+          Player 3
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+      </div>
+      <div id="player4" class="teamatesHand">
+        <div>
+          Player 4
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+        <div class="teamatesCard">
+          <h3>
+            Color
+          </h3>
+          <h4>
+            Number
+          </h4>
+        </div>
+      </div>
+      </div>
   )
 }
 
