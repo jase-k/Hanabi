8bbf22bc57c7111fc6252e17f66824178433f531
Glitch (hello-sqlite) <none>
2019-01-25 12:26:25 +0000

    😬📍 Checkpoint ./server.js:1326932/182

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const gameCreation = require('./cool-file.js')
 const Database = require('./database.js')
@@ -12,9 +12,7 @@ const ReactDOM = require('react-dom')
 const React = require('react')
 const colors = ['white', 'red', 'black', 'orange', 'blue']
 
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const cors =require('c
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
