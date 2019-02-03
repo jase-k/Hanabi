a1a3c9f6f9f5be21016f980705426596b4094e59
Glitch (puddle-catcher) <none>
2019-02-03 21:28:16 +0000

    🎮🐴 Checkpoint ./test/test.js:1326932/419 ./package.json:1326932/27

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+
+describe("cool-file", function(){
+  describe("New Game Configuration", function(){
+    it("Should return a complete game Object", function(){
+      console.log("Mocha is connected")
+      assert.ok(true);
+    });
+  });
+});
