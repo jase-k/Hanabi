ce2952d3861474840a18405b0fbf9febd374a7b9
Glitch (puddle-catcher) <none>
2019-02-03 22:01:14 +0000

    🙅💁 Checkpoint ./test/test.js:1326932/445 ./game-play.js:1326932/91

--- test/test.js
+++ test/test.js
@@ -13,10 +13,16 @@ describe("GamePlay", function(){
     
     it("Should returns a complete game Object for 2 people", function(){
       const numberOfPlayers = 2; 
-      const expectObjectKeys = ["id", "numberOfPlayers", "score", "dateCreated", "hintsLeft", "livesLeft", "originalDeck", "playingDeck", 
-                                "players", "messages", "discardedCards", "playedCards", "]
+      const expectedObjectKeys = ["numberOfPlayers", "score", "dateCreated", "hintsLeft", "livesLeft", "originalDeck", "playingDeck", 
+                                "players", "messages", "discardedCards", "playedCards", "tableIds"]
       
-      GamePlay.newGame(numberOfPlayers)
+      const newGameObject = GamePlay.newGame(numberOfPlayers)
+      const newGameKeys = Object.keys(newGameObject)
+      newGameKeys.sort();
+      expectedObjectKeys.sort();
+
+      assert.deepEqual(expectedObjectKeys, newGameKeys)
+      
       
     });
   
