a5898215600e30dfea77a2d44e67a8cb1ec7b238
Glitch (puddle-catcher) <none>
2019-02-05 14:11:07 +0000

    🦁⛺️ Checkpoint ./test/test.js:1326932/1809

--- test/test.js
+++ test/test.js
@@ -5,7 +5,26 @@ const assert = require('chai').assert
 
 const GamePlay = require('../game-play.js');
 
-
+const defaultGameSettings = {
+                dateCreated: new Date(), 
+                discardedCard: [],
+                hintsLeft: 8, 
+                id: 0, 
+                livesLeft: 3, 
+                messages: [],
+                numberOfPlayers: 3,
+                originalDeck: [/*Length of 50 Card Objects*/],
+                playedCards: [],
+                players: [
+                  {
+                    id: 0,
+                    name: "John",
+                    active: 0, 
+                    hand: [],
+                  }
+                ],
+                
+              }
 
 describe("GamePlay", function(){
   
@@ -13,8 +32,7 @@ describe("GamePlay", function(){
     
     it("Should returns a complete game Object for 2 people", function(){
       const numberOfPlayers = 2; 
-      const expectedObjectKeys = ["numberOfPlayers", "score", "dateCreated", "hintsLeft", "livesLeft", "originalDeck", "playingDeck", 
-                                "players", "messages", "discardedCards", "playedCards", "tableIds"]
+      const expectedObjectKeys = Object.keys(defaultGameSettings)
       
       const newGameObject = GamePlay.newGame(numberOfPlayers)
       const newGameKeys = Object.keys(newGameObject)
@@ -26,21 +44,16 @@ describe("GamePlay", function(){
       
     });
   
-    it("Should return a complete game Object for 4 people", function(){
-        const numberOfPlayer = 4;
+    it("s Player's Array's Length should be the same as number of Players", function(){
+        const numberOfPlayers = 4;
       
+        const newGameObject = GamePlay.newGame(numberOfPlayers)
       
       
     });
     
     it("Should set Game to Default Settings", function(){
-      const defaultSettings = {
-                dateCreated: new Date(), 
-                discardedCard: [],
-                hintsLeft: 8, 
-                livesLeft: 3, 
-                
-              }
+      
       
     });
   
