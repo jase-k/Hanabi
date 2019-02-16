70c1d6ace25d391df8ae1b6832c70419de4553f7
Glitch (puddle-catcher) <none>
2019-02-16 15:56:53 +0000

    🌈🏊🏼 Checkpoint ./test/api_test.js:1326932/516

--- test/api_test.js
+++ test/api_test.js
@@ -7,4 +7,19 @@ https://puddle-catcher.glitch.me/
 
 */
 
-describe("NEW GAME: /newgame/:numberOfPlayers?name=NAME", function()
+describe("NEW GAME: /newgame/:numberOfPlayers?name=NAME", function(){
+  it("should add a new game to the database", function(){
+    const xhr = new XMLHttpRequest
+    var url = 'https://puddle-catcher.glitch.me/newgame/2/Jase'
+    
+      xhr.responseType= "json";
+      xhr.onreadystatechange = () => {
+        if(xhr.readState === XMLHttpRequest.DONE){
+          console.log(xhr.response)
+        }
+      }
+    xhr.open('GET', url)
+    xhr.send()
+    
+    })
+});
