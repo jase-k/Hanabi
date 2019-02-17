d0f601574e5398f85d0e949254a147a15c72cc4a
Glitch (puddle-catcher) <none>
2019-02-17 23:02:56 +0000

    🏛☕️ Checkpoint ./test/api_test.js:1326932/460

--- test/api_test.js
+++ test/api_test.js
@@ -6,16 +6,26 @@ This document test the API Call from:
 https://puddle-catcher.glitch.me/
 
 */
+//Import dependencies
 const rp = require('request-promise');
+const assert = require('chai').assert;
 
+//Import Local Modules
 const Defaults = require('./defaults');
 
 describe("NEW GAME: /newgame/:numberOfPlayers?name=NAME", function(){
   it("should add a new game to the database", function(done){
     var game = Defaults.gameSettings2Player();
-    var url = 'https://puddle-catcher.glitch.me/newgame/2?name=Frodo"
+    var url = 'https://puddle-catcher.glitch.me/newgame/2?name=Frodo'
     
-    rp()
+    rp(url)
+    .then(function(results){
+      console.log(results)
+      assert.equal(results.numberOfPlayers, 2)
+      done();
+      assert.ok(results)
+      
+    })
     
 
     
