df59de79cc276912f06e55eeb4c1a318caf84881
Glitch (puddle-catcher) <none>
2019-02-17 00:07:51 +0000

    🙋🛋 Checkpoint ./.glitch-assets:1326932/6621 ./assets/gifs.js:1326932/1090

---
+++ assets/gifs.js
@@ -0,0 +1,27 @@
+/*
+This Document stores Winning Gifs to send to the winners after a completed game
+
+*/
+
+const WinningGifs = [
+  {
+    url: "https://cdn.glitch.com/2f645685-fc7e-4ed3-a61f-f16db85bfe59%2Fcat.gif?1550361604356",
+    alt: "You Cat to be Kitten Me! You Won!"
+  },
+  {
+    url: "https://cdn.glitch.com/2f645685-fc7e-4ed3-a61f-f16db85bfe59%2Fpenguins.gif?1550361604471",
+    alt: "Play it cool.. but you Just won."
+  },
+  {
+    url:"https://cdn.glitch.com/2f645685-fc7e-4ed3-a61f-f16db85bfe59%2Fcat1.gif?1550361606031",
+    alt: "Go ahead, get up and do a little jig, because you are a winner!"
+  },
+  {
+    url: "https://cdn.glitch.com/2f645685-fc7e-4ed3-a61f-f16db85bfe59%2Fseal.gif?1550361606336", 
+    alt: "How does it seal to be a winner?! Ask yourself, because that's what you are."
+  },
+  {
+    url: "https://cdn.glitch.com/2f645685-fc7e-4ed3-a61f-f16db85bfe59%2Fduck1.gif?1550361607026",
+    alt: "Go A"
+  }
+]
