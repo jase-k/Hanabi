3bf066a61db04f060d80e34c9f2eacbc870c806d
Glitch (puddle-catcher) <none>
2019-02-17 23:34:08 +0000

    👀🎤 Checkpoint ./test/api_test.js:1326932/4573

--- test/api_test.js
+++ test/api_test.js
@@ -11,25 +11,46 @@ const rp = require('request-promise');
 const assert = require('chai').assert;
 
 //Import Local Modules
-const Defaults = require('./defaults');
+//Import Database
+var fs = require('fs');
+var dbFile = './.data/sqlite.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+
+//Import Module:
+const {Database, Utils, Helper} = require('../modules/database.js')
+const Defaults = require('./defaults.js')
 
 describe("NEW GAME: /newgame/:numberOfPlayers?name=NAME", function(){
   it("should add a new game to the database", function(done){
-    const expectedObjectKeys = ["numberOfPlayers, "],
-                         url = 'https://puddle-catcher.glitch.me/newgame/2?name=Frodo'
+    const expectedObjectKeys = [ 'dateCreated',  'discardedCards', 'hintsLeft', 
+                                'livesLeft', 'messages', 'numberOfPlayers', 'originalDeck', 'playedCards',
+                                'players', 'playingDeck', 'score', 'tableIds']
+    
+    var url = 'https://puddle-catcher.glitch.me/newgame/2?name=Frodo'
     
     rp(url)
     .then(function(results){
-      console.log(results)
+        after(function(){
+            db.run("DELETE FROM HanabiGames WHERE id = "+results.tableIds.gameId)
+            db.run("DELETE FROM OriginalDeck WHERE gameId = "+results.tableIds.gameId)
+            db.run("DELETE FROM PlayingDeck WHERE gameId = "+results.tableIds.gameId)
+            db.run("DELETE FROM DiscardedCards WHERE gameId = "+results.tableIds.gameId)
+            db.run("DELETE FROM PlayedCards WHERE gameId = "+results.tableIds.gameId)
+            db.run("DELETE FROM Messages WHERE gameId = "+results.tableIds.gameId)
+            db.run("DELETE FROM Players WHERE gameId = "+results.tableIds.gameId)
+         });
+      
       var object = JSON.parse(results)
       var objectKeys = Object.keys(object)
-      assert.deepEqual(objectKeys, expectedObjectKeys, ""+objectKeys+" should equal "+expectedObjectKeys)
-      assert.ok(results)
-      done();
       
+      assert.deepEqual(objectKeys, expectedObjectKeys, ""+objectKeys+" should equal "+expectedObjectKeys)
+      assert.equal(object.players[0].name, 'Frodo')
+      assert.ok(results)
+      
+      done();
     })
-    
-
-    
-    })
+  })
 });
